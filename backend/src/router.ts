a97c766af6e8b550556ca4e7ee07edc1a4949349
renan-mion <rnunesmion@gmail.com>
2025-01-11 14:21:55 -0300

    feat: instalado prisma

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response, response } from "express";
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({nome: "PizzaFlow"});
+    return res.json({
+        teste: 'OK'
+    })
 });
 
 export { router };

7fbaa892ecfdcd2ed8e4874c7432b679cd988d68
renan-mion <rnunesmion@gmail.com>
2025-01-27 17:34:21 -0300

    feat: adicionado cadastro

---
+++ frontend/src/app/cadastro/page.tsx
@@ -0,0 +1,55 @@
+import styles from '../page.module.scss';
+import Logo from '/public/Logo.png';
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+export default function Cadastro() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.containerCenter}>
+                <Image
+                    src={Logo}
+                    alt='Logo do app'
+                    width={600}
+                    height={200}
+                    className={styles.logo}
+                />
+
+                <section className={styles.login}>
+                    <form>
+                        <input
+                            type="name"
+                            required
+                            name='name'
+                            placeholder='Digite seu nome...'
+                            className={styles.input}
+                        />
+
+                        <input
+                            type="email"
+                            required
+                            name='email'
+                            placeholder='Digite seu email...'
+                            className={styles.input}
+                        />
+
+                        <input type="password"
+                            required
+                            name='password'
+                            placeholder='**********'
+                            className={styles.input}
+                        />
+
+                        <button type='submit' className={styles.button}>Acessar</button>
+
+                        <Link href="/" className={styles.text}>
+                            Já uma conta? faça o login
+                        </Link>
+                    </form>
+                </section>
+            </div>
+        </main>
+    )
+}

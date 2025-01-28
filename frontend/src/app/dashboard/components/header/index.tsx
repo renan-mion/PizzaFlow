91c8a33df343e966ebd6513f29118f92359271eb
renan-mion <rnunesmion@gmail.com>
2025-01-28 16:54:08 -0300

    feat: criando header

---
+++ frontend/src/app/dashboard/components/header/index.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import Logo from '/public/Logo.png';
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { LogOutIcon } from 'lucide-react';
+import { deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+
+export function Header() {
+    const router = useRouter();
+    
+    async function handleLogOut() {
+        deleteCookie("session", { path: "/" });
+
+        router.replace("/");
+    }
+
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Link href="/">
+                    <Image src={Logo} alt='Logo' priority={true} quality={100} className={styles.logo} />
+                </Link>
+
+                <nav>
+                    <Link href="dashboard/categoria">Nova Categoria</Link>
+                    <Link href="dashboard/produtos">Cardápio</Link>
+
+                    <form action={handleLogOut}>
+                        <button type='submit'>
+                            <LogOutIcon size={24} color="#FFF"/>
+                        </button>
+                    </form>
+                </nav>
+            </div>
+        </header>
+    )
+}
---
+++ frontend/src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/header"
+
+export default function DashboardLayout({ children }: 
+    { children: React.ReactNode}
+) {
+    return (
+        <div>
+            <Header/>
+            {children}
+        </div>
+    )
+}
